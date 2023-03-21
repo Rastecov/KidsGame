c0201d0fddce192c87fefcca97a47bbc6d411f9c
Eraste Yacov <113373188+Rastecov@users.noreply.github.com>
2023-03-21 10:27:55 -0400

    Level 1 and 2

--- Level 1.php
+++ Level 1.php
@@ -1,38 +1,50 @@
 <?php
 // Check if form has been submitted
-if (isset($_POST['numbers'])) {
+if (isset($_POST['letters'])) {
   // User has submitted the form, check if numbers are in ascending order
-  $input_numbers = explode(",", $_POST['numbers']);
-  $input_numbers = array_map('trim', $input_numbers);
+  $input_letters = explode(",", $_POST['letters']);
+  $input_letters = array_map('trim', $input_letters);
 
-  if (count($input_numbers) != 6) {
-    echo "<p>Please enter 6 numbers, separated by commas.</p>";
+  if (count($input_letters) != 6) {
+    echo "<p>Please enter 6 letters, separated by commas.</p>";
   } else {
-    $sorted_input = implode(", ", $input_numbers);
-    sort($input_numbers);
 
-    if ($sorted_input === implode(", ", $input_numbers)) {
-      echo "<p>You have won the game!</p>";
-    } else {
-      echo "<p>Sorry, the numbers you entered did not match the sorted numbers. Please try again.</p>";
-    }
+    $sorted_letterInput = implode(", ", $input_letters);
+    sort($input_letters);
+
+    echo $input_letters. "<br/>"; 
+
+    if ($sorted_letterInput === implode(", ", $input_letters)) {
+        echo "<p>You have won the game!</p>";
+      } else {
+        echo "<p>Sorry, the numbers you entered did not match the sorted numbers. Please try again.</p>";
+      }
+
+
+   
   }
 } else {
-  // Generate 6 random numbers from 0-100
-  $numbers = array();
-  for ($i = 0; $i < 6; $i++) {
-    $numbers[] = rand(0, 100);
-  }
-  $sorted_numbers = implode(", ", $numbers);
+  // Generate 6 random letters from the alphabet
+   $n = 6;
+    
+    $characters = 'abcdefghijklmnopqrstuvwxyz';
+    $randomString = '';
+     
+    for ($i = 0; $i <$n; $i++) {
+       $index = rand(0, strlen($characters) - 1);
+       $randomString .= $characters[$index];
+    }
+     
 
-  // Show the numbers to the user
-  echo "<p>The random numbers are: $sorted_numbers</p>";
+  // Show the letters generated to the user
+    echo "The random letters generated are : "."<br/>".$randomString."<br/>"; 
+     
 
   // Show the form to the user
   ?>
   <form method="post" action="">
-    <label for="numbers">Enter the 6 numbers in ascending order:</label>
-    <input type="text" id="numbers" name="numbers">
+    <label for="letters">Enter the 6 letters in ascending order:</label>
+    <input type="text" id="letters" name="letters">
     <button type="submit">Submit</button>
   </form>
   <?php
