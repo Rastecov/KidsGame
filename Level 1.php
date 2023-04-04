1217b13652edca64db4b7e1ef79ebf5b1d8706ca
Wyc123888888 <112558189+Wyc123888888@users.noreply.github.com>
2023-04-04 10:53:23 -0400

    Update Level 1.php

--- Level 1.php
+++ Level 1.php
@@ -1,66 +1,57 @@
-
-
 <?php
-// Check if form has been submitted
-
 require "header.php";
-if (isset($_POST['letters'])) {
-  // User has submitted the form, check if numbers are in ascending order
-  $input_letters = explode(",", $_POST['letters']);
-  $input_letters = array_map('trim', $input_letters);
 
-  if (count($input_letters) != 6) {
-    echo "<p>Please enter 6 letters, separated by commas.</p>";
-  } else {
-
-    $sorted_letterInput = implode(", ", $input_letters);
-    sort($input_letters);
-
-    echo $input_letters. "<br/>"; 
-
-    if ($sorted_letterInput === implode(", ", $input_letters)) {
-        echo "<p>You have won the game!</p>";
-      } else {
-        echo "<p>Sorry, the numbers you entered did not match the sorted numbers. Please try again.</p>";
-      }
-
-
-   
-  }
-} else {
-  // Generate 6 random letters from the alphabet
-   $n = 6;
-    
-    $characters = 'abcdefghijklmnopqrstuvwxyz';
-    $randomString = '';
-     
-    for ($i = 0; $i <$n; $i++) {
-       $index = rand(0, strlen($characters) - 1);
-       $randomString .= $characters[$index];
-       if ($i < $n - 1) {
-        $randomString .= ", ";
-       }
-
-    }
-     
-
-  // Show the letters generated to the user
-    echo "<p>The random letters generated are : $randomString <p>"; 
-     
-
-  // Show the form to the user
-  ?>
-  <form method="post" action="">
-    <label for="letters">Enter the 6 letters in ascending order:</label>
-    <input type="text" id="letters" name="letters">
-    <button type="submit">Submit</button>
-  </form>
-
-  
-  <?php
+function redirectToLevel2() {
+    echo '<form method="post" action="level2.php">
+        <button type="submit">Proceed to Level 2</button>
+    </form>';
 }
 
-echo' <form action="includes/logout.php" method="post">
+if (isset($_POST['letters'])) {
+    $input_letters = explode(",", $_POST['letters']);
+    $input_letters = array_map('trim', $input_letters);
+
+    if (count($input_letters) != 6) {
+        echo "<p>Please enter 6 letters, separated by commas.</p>";
+    } else {
+        $sorted_input_letters = $input_letters;
+        sort($sorted_input_letters);
+        $sorted_input_letters_str = implode(", ", $sorted_input_letters);
+
+        echo "Your input: " . implode(", ", $input_letters) . "<br/>";
+
+        if ($sorted_input_letters_str === implode(", ", $input_letters)) {
+            echo "<p>You have won the game!</p>";
+            redirectToLevel2();
+        } else {
+            echo "<p>Sorry, the numbers you entered did not match the sorted numbers. Please try again.</p>";
+        }
+    }
+} else {
+    $n = 6;
+
+    $characters = 'abcdefghijklmnopqrstuvwxyz';
+    $randomString = '';
+
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+        if ($i < $n - 1) {
+            $randomString .= ", ";
+        }
+    }
+
+    echo "<p>The random letters generated are: $randomString</p>";
+?>
+    <form method="post" action="">
+        <label for="letters">Enter the 6 letters in ascending order:</label>
+        <input type="text" id="letters" name="letters">
+        <button type="submit">Submit</button>
+    </form>
+<?php
+}
+
+echo '<form action="includes/logout.php" method="post">
 <button class="logout-btn" type="submit" name="logout-submit">Logout</button>
 </form>';
 ?>
