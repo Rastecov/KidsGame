9d392b6cfb02e23d06e999f749289334b0330757
Eraste Yacov <113373188+Rastecov@users.noreply.github.com>
2023-04-07 16:15:38 -0400

    Login updated

--- includes/Login2.php
+++ includes/Login2.php
@@ -1,36 +1,39 @@
 <?php
-if(isset($_POST['login-submit'])){
-   
-     // get the form data
-     $username = $_POST['username'];
-     $password = $_POST['pwd'];
- 
-        
-     if (empty($username) || empty($password)) {
+require 'database_handler.php';
 
-      header("Location: ../index.php?error=emptyfields&uid=" . $username);
+if (isset($_POST['login-submit'])) {
 
-      exit();
+    // get the form data
+    $username = $_POST['username'];
+    $password = $_POST['pwd'];
+
+    if (empty($username) || empty($password)) {
+        header("Location: ../index.php?error=emptyfields&uid=" . $username);
+        exit();
     }
-        
-        // query the database for the user
-        $sql = "SELECT * FROM users WHERE userName='$username' AND userpassword='$password'";
-        $result = mysqli_query($conn, $sql);
-        
-        // check if the user was found
-        if (mysqli_num_rows($result) == 1) {
-          // log the user in
-          session_start();
-          $_SESSION['username'] = $username;
-          header("Location: ../Level 1.php");
-          exit();
-        } else {
-          // display an error message
-          
-          header("Location: ../index.php?error=Invalid ID or Password");
-          
-        }
-        
-        mysqli_close($conn);
-      }
-    
+
+    // query the database for the user
+    $sql = "SELECT player.userName, authenticator.passCode
+            FROM player
+            INNER JOIN authenticator ON player.registrationOrder = authenticator.registrationOrder
+            WHERE player.userName = '$username' AND authenticator.passCode = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    // check if the user was found
+    if ($result->num_rows > 0) {
+        // log the user in
+        session_start();
+        $_SESSION['username'] = $username;
+        header("Location: ../Level 1.php");
+        exit();
+    } else {
+        // display an error message
+        header("Location: ../index.php?error=Invalid ID or Password");
+        exit();
+    }
+
+    mysqli_close($conn);
+} else {
+    header("Location: ../index.php");
+    exit();
+}
